db989fc4fb073a755d0ada19be898b2b671fd821
Hinkley <Se.G.Hinkley@baypath.net>
2024-03-15 14:20:16 -0400

    first Commit

---
+++ src/Render/MyGame.java
@@ -0,0 +1,62 @@
+package Render;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class MyGame extends Game  {
+    public static final String TITLE = "Platformer";
+    public static final int SCREEN_WIDTH = 1700;
+    public static final int SCREEN_HEIGHT = 900;
+    
+    
+
+
+    public MyGame() 
+    {
+
+    }
+    
+    public void update() 
+    {
+    	
+    }
+    
+    public void draw(Graphics pen) 
+    {
+
+    }
+        
+    @Override
+    public void keyTyped(KeyEvent ke) {}
+
+    @Override
+    public void keyPressed(KeyEvent ke) 
+    {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {}
+
+    @Override
+    public void mouseClicked(MouseEvent ke) {}
+
+    @Override
+    public void mousePressed(MouseEvent me) {}
+    
+    @Override
+    public void mouseReleased(MouseEvent me) {}
+
+    @Override
+    public void mouseEntered(MouseEvent me) {}
+
+    @Override
+    public void mouseExited(MouseEvent me) {}
+        
+        
+    //Launches the Game
+    public static void main(String[] args) { new MyGame().start(TITLE, SCREEN_WIDTH,SCREEN_HEIGHT); }
+}
